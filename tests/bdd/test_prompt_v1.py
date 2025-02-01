a49bf860d3c3c3d50b45439b8ea0ee6eaf7ccc42
Fabiano Teichmann <fabiano.geek@gmail.com>
2025-02-01 01:54:32 -0300

    improve tests

--- tests/bdd/test_prompt_v1.py
+++ tests/bdd/test_prompt_v1.py
@@ -4,6 +4,7 @@ import pytest
 class TestPromptV1:
     """Classe de testes para DataFrame com pytest"""
 
+
     @pytest.fixture(autouse=True)
     def setup(self):
         """Carrega o DataFrame antes de cada teste"""
@@ -12,10 +13,34 @@ class TestPromptV1:
 
     def test_given_llm_receives_one_record_invalid_then_should_return_one_less(self):
         """Verifica se o DataFrame não está vazio"""
-        assert self.df_bronze.nickname.count() + 1 == self.df_raw["Qual o @ (identifica a rede)"].count()
+        assert self.df_bronze.nickname.count() == self.df_raw["Qual o @ (identifica a rede)"].count()
         assert not self.df_bronze.empty, "O DataFrame está vazio!"
 
 
-    def test_dataframe_not_be_return_invalid_data(self):
-        """Não deve encontrar influencer com review com texto ilegivel"""
-        assert self.df_bronze[self.df_bronze["nickname"] == "@dragbox"].empty
+    def test_dataframe_not_be_return_sentiment_with_undefined(self):
+        assert (self.df_bronze[self.df_bronze["nickname"] == "@dragbox"]["personality"] == "undefined").all()
+        assert (self.df_bronze[self.df_bronze["nickname"] == "@dragbox"]["professional"] == "undefined").all()
+
+
+
+    @pytest.fixture(params=[
+        ("positive", ("@gildovigor", "@tchulim - Instagram"))
+    ])
+    def reviews_positive(self, request):
+        return request.param
+
+    def test_should_return_review_positive_personality_and_professional(self, reviews_positive):
+        review, nick_name = reviews_positive
+        assert (self.df_bronze[self.df_bronze["nickname"] == nick_name]["personality"] == "positive").all()
+        assert (self.df_bronze[self.df_bronze["nickname"] == nick_name]["professional"] == "positive").all()
+
+    @pytest.fixture(params=[
+        ("positive", ("blogueirinha", "@mohindi"))
+    ])
+    def reviews_negative(self, request):
+        return request.param
+
+    def test_should_return_review_negative_personality_and_professional(self, reviews_negative):
+        review, nick_name = reviews_negative
+        assert (self.df_bronze[self.df_bronze["nickname"] == nick_name]["personality"] == "negative").all()
+        assert (self.df_bronze[self.df_bronze["nickname"] == nick_name]["professional"] == "unprofessional").all()
